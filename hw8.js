e723b0dbf9860a8122e14d899514cb13bc5ba21b
chrislupdx <christopherlu.pdx@gmail.com>
2021-11-24 12:54:01 -0800

    successfully adding the closest city to the mst

--- hw8.js
+++ hw8.js
@@ -40,38 +40,48 @@ function prims(adjList)
 {
     let data = adjList;
     const keys = Object.keys(data); //grabs a random vertex
-    const rand_vertex = keys[Math.floor(Math.random() * keys.length)];
+    const rand_city = keys[Math.floor(Math.random() * keys.length)];
 
     //how are you representing your MST?
     let mst = new Object(); //initialize mst with a random vertex, mark that vertex as visited
 
-    mst[rand_vertex] = adjList[rand_vertex];
-    data[rand_vertex].visited = true; //mark our starting vertex on the adjacency list
+    mst[rand_city] = adjList[rand_city];
+    data[rand_city].visited = true; //mark our starting vertex on the adjacency list
 
     //find the array with the shortest path/edge around the MST
     let shortest = Number.POSITIVE_INFINITY;
-
     //search a single vertex in the MST(we need to scale this for all vertices in the mst)
     for(vertex in mst)
     {
-        for(let i = 0; i < mst[rand_vertex].length; i++)
+        for(let i = 0; i < mst[rand_city].length; i++)
         {
-            shortest = Math.min(shortest, mst[rand_vertex][i][3]);
+            shortest = Math.min(shortest, mst[rand_city][i][3]);
         }
     }
 
     let shortest_vertex;
+    let next_city;
     //grab the vertex+path we just found
-    for(let i = 0; i < mst[rand_vertex].length; i++) //wait by mst?
+    for(let i = 0; i < mst[rand_city].length; i++) //wait by mst?
     {
-        if(mst[prop][i][3] == shortest)
+        if(mst[rand_city][i][3] == shortest)
         {
-            console.log(mst[prop][i]);
-            //add the corresponding city to mst as a definition
+            data[rand_city].visited = true;
+            next_city = mst[rand_city][i][2]; //grab the name of our next city
+            //let next_city = mst[rand_city][i][2]; //grab the name of our next city
+            mst[next_city] = data[next_city]; //create a key,val pair in mst(add the city)
             //mark the right definition in data as visitited
+
+            //mark in rand_city's path to next_city as 1
+            //mark in next_city's path to rand_city's path as 1
+
+
         }
     }
-
+    console.log("mst is ");
+    console.log(mst);
+    console.log("rand city ", rand_city);
+    console.log("next city ", next_city);
 }
 
 //scans for shortest vertex 
