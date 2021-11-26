67f6304439cbfeeb331a3a4b7baf388f2bcb923e
chrislupdx <christopherlu.pdx@gmail.com>
2021-11-26 14:04:17 -0800

    cleanup

--- hw8.js
+++ hw8.js
@@ -59,23 +59,25 @@ function prims(adjList)
     const first_city = keys[Math.floor(Math.random() * keys.length)];
     MST[first_city] = adjList[first_city];
     adjList[first_city].visited = true;
-    
-    for(let i = 0; i < 4; i++) //the real stopping condition is when mst lenght == adjlist.length -1 
+    console.log("keys are", keys.length);
+    for(let i = 0; i < (keys.length - 1); i++) //the real stopping condition is when mst lenght == adjlist.length -1 
     {
-    //the stuff below keeeps looping until we fill the mst
-    let shortest_path_city_to;
-    let shortest_path_city_from;
-    let shortest_len = findShortestPath(MST);
-    addVertex(MST, adjList, shortest_path_city_to, shortest_path_city_from, shortest_len);
-    console.log(Object.keys(MST).length);
+        //the stuff below keeeps looping until we fill the mst
+        let shortest_path_city_to;
+        let shortest_path_city_from;
+        let shortest_len = findShortestPath(MST);
+        addVertex(MST, adjList, shortest_path_city_to, shortest_path_city_from, shortest_len);
+        console.log(Object.keys(MST).length);
     }
     //what do we do with the adjacency list
+//    console.log(MST);
 }
 
 function addVertex(MST, adjList, shortest_path_city_to, shortest_path_city_from, shortest_len)
 {
     for(vertex in MST)
     {
+        //console.log("vertex in loop is", vertex);
         for(let i = 0; i < MST[vertex].length; i++)
         {
             if(MST[vertex][i][3] == shortest_len && MST[vertex][i][0] == null)
@@ -83,19 +85,20 @@ function addVertex(MST, adjList, shortest_path_city_to, shortest_path_city_from,
                 MST[vertex][i][0] = 1; //marked off the path from -> to
                 shortest_path_city_to = MST[vertex][i][2]; //something feelsl ike this should occur in separately
                 shortest_path_city_from = MST[vertex][i][1];
-
+                //console.log(shortest_path_city_from, "spcf in loop");
                 adjList[shortest_path_city_to].visited = true;
                 adjList[shortest_path_city_from].visited = true;
 
                 MST[shortest_path_city_to] = adjList[shortest_path_city_to];
+                console.log(MST[shortest_path_city_to].length, "spct in loop");
                 MST[shortest_path_city_from] = adjList[shortest_path_city_from];
                 break;
             }
         }
     }
-    
-    console.log(MST);
-    console.log("in av shortest path city to is", shortest_path_city_to, MST[shortest_path_city_to]);
+
+    //console.log(MST);
+    //console.log("in av shortest path city to is", shortest_path_city_to, MST[shortest_path_city_to]);
 
     //mark off the path to -> from
     for(let i = 0; i < MST[shortest_path_city_to].length; i++)
@@ -114,7 +117,10 @@ function findShortestPath(mst)
     {   
         for(let i = 0; i < mst[vertex].length; i++)
         {
-            shortest_len = Math.min(shortest_len, mst[vertex][i][3]);
+            if(mst[vertex][i][0] != 1) //and its unvisited
+            {
+                shortest_len = Math.min(shortest_len, mst[vertex][i][3]); //TODO  we could mark it off here actually
+            }
         }
     }
     return shortest_len;
