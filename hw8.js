41eb463dff47eb435626465735f2a0300873f252
chrislupdx <christopherlu.pdx@gmail.com>
2021-11-29 14:38:16 -0800

    finally have displays that actually diagnose parts of the issue

--- hw8.js
+++ hw8.js
@@ -37,9 +37,11 @@ function hasVertex(MST, name)
     {
         if(name == vertex)
         {
+            //console.log("name is ", name, "vertex is ", vertex);
             return true
         }
     }
+    //console.log(name, " is not in MST atm");
     return false;
 }
 function prims(adjList)
@@ -52,30 +54,27 @@ function prims(adjList)
     for(let i = 0; i < keys.length; i++) //the real stopping condition is when mst lenght == adjlist.length -1  //TODO this stopping conidition is suspciious
     {
         let shortest_len = findShortestPath(MST); //put adjList in here TODO
-        console.log("in prims shortest is", shortest_len);
         totalweight += shortest_len;
         addVertex(MST, adjList, shortest_len);
     }
-    //console.log(MST, Object.keys(MST).length, "is keys", totalweight, " total weight");
+    console.log(MST, Object.keys(MST).length, "is keys", totalweight, " total weight");
 }
-
 function addVertex(MST, adjList, shortest_len)
 {
     let shortest_path_city_to;
     let shortest_path_city_from;
-
-    //add shortest_path_city_to into MST TODO, is the only way to do a weird lookup by edge? is there some way to assign it
     for(vertex in MST) 
     {
-        for(let i = 0; i < MST[vertex].length; i++)
+        for(let i = 0; i < (MST[vertex].length - 1); i++)
         { 
-            if(MST[vertex][i][3] == shortest_len && hasVertex(MST, MST[vertex][i][2]) == false) //and the name of the the vertex we are connecting to does not exist in the mst
+            //if(MST[vertex][i][3] == shortest_len) //and the name of the the vertex we are connecting to does not exist in the mst
+            if(MST[vertex][i][3] == shortest_len && hasVertex(MST, MST[vertex][i][2]) === false) //and the name of the the vertex we are connecting to does not exist in the mst
             {
                 console.log("shortest len_to is", MST[vertex][i][2]);
                 MST[vertex][i][0] = 1; //marked off the path from -> to
                 shortest_path_city_to = MST[vertex][i][2]; 
                 shortest_path_city_from = MST[vertex][i][1]; 
-                adjList[shortest_path_city_to].visited = true; //TODO are both necessary?
+                adjList[shortest_path_city_to].visited = true;
                 adjList[shortest_path_city_from].visited = true;
                 MST[shortest_path_city_to] = adjList[shortest_path_city_to];
                 MST[shortest_path_city_from] = adjList[shortest_path_city_from];
@@ -91,29 +90,30 @@ function addVertex(MST, adjList, shortest_len)
         }
     }
 }
-//if we brought in the base map, could it help us
-function findShortestPath(mst)
+function findShortestPath(mst) //check if we are saving and comparing between vertices
 {
-    let shortest_len = Number.POSITIVE_INFINITY;
-    let fail = true; //can we flag for when we don't progress any further? TODO 
+    console.log("START");
+    let shortest_len = Number.POSITIVE_INFINITY; //what if we compiled a list of locations?
     let shortest_len_city_from;
     let shortest_len_city_to;
-    for(vertex in mst)
+    for(vertex in mst) 
     {  
-        for(let i = 0; i < mst[vertex].length; i++)
+        console.log(shortest_len, " is current shortest len");
+        for(let i = 0; i < (mst[vertex].length - 1); i++)
         { 
-            if(hasVertex(mst, mst[vertex][i][2]) == false && mst[vertex][i][0] != 1) //and its unvisited path and vertex
+            
+            if((hasVertex(mst, mst[vertex][i][2]) === false) && mst[vertex][i][0] != 1) //and its unvisited path and vertex
             {
-                //console.log(mst[vertex][i], " is shortest length city");
-                shortest_len_city_to = mst[vertex][i][2];
-                shortest_len_city_from = mst[vertex][i][1];
-                shortest_len = Math.min(shortest_len, mst[vertex][i][3]); 
+                shortest_len_city_to = mst[vertex][i][2]; 
+                shortest_len_city_from = mst[vertex][i][1]; 
+                let curr_shortest_len = mst[vertex][i][3];
+                shortest_len = Math.min(shortest_len, curr_shortest_len); 
+                console.log("considering  slc_from: ", shortest_len_city_from, "slc_to ", shortest_len_city_to, "that length is", curr_shortest_len,  " BUT shortest len is ", shortest_len);
                 mst[vertex].visited = true;
-                fail = false;
             } 
         }
     }
-    console.log('shortest len city is from', shortest_len_city_from, "shortest len city is to ", shortest_len_city_to);
+    console.log('FINAL shortest len city is from', shortest_len_city_from, "shortest len city is to ", shortest_len_city_to, " of lengtth " , shortest_len, "slct status in mst ", hasVertex(mst, shortest_len_city_to));
     return shortest_len;
 }
 let data = parseText();
